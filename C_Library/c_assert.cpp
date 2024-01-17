db4e312bff18fe0270b574b992c6d4637d7e9354
Saad Ahmed <saad.24.ahm@gmail.com>
2024-01-17 13:17:49 +0000

    used ctype.h except for last two methods

--- C_Library/c_assert.cpp
+++ C_Library/c_assert.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <assert.h>
 
-// Assertion failed: expression, file filename, line line number
-// Assertion failed: helloWorld == "", file assert.cpp, line 8
-
 int main() {
     std::string helloWorld = "Hello World!";
     assert(helloWorld == "");
     return 0;
 }
+
+/*
+
+Notes:
+- returns: Assertion failed: expression, file filename, line line number
+- Assertion failed: helloWorld == "", file assert.cpp, line 8
+
+*/
