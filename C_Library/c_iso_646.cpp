f0d74fcc57ba493aa95f58bd0e8793acf7e5ba1b
Saad Ahmed <saad.24.ahm@gmail.com>
2024-01-18 15:16:11 +0000

    learned about iso646.h

---
+++ C_Library/c_iso_646.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iso646.h>
+
+int main () 
+{
+    int x = 5, y = 10;
+
+    if (x and y) {
+        printf("x and y are both true.\n");
+    }
+
+    return 0;
+}
+
+/*
+
+Notes:
+- Just used for alternative spelling of operators (x and/&& y) in c not supported by ISO646 standard character set
+- Isn't neccessary if you want to use alternative operator names in c++
+
+*/
