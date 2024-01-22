16e7d9c9482a03e07aedea4329b7cf29ca757f3f
Saad Ahmed <saad.24.ahm@gmail.com>
2024-01-22 19:44:25 +0000

    learned setjmp.h

---
+++ C_Library/c_set_ump.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <setjmp.h>
+
+// Hold information to restore calling environment)
+jmp_buf jumpBuffer;
+
+void process1() 
+{
+    std::cout << "Process1 started" << std::endl;
+
+    //setjmp defines state. If it's not 0, longjmp will be called. 
+    if(setjmp(jumpBuffer) != 0)
+    {
+        std::cout << "process1 after longjmp" << std::endl;
+        return;
+    }
+    std::cout << "process 1 jumping to process2" << std::endl;
+    longjmp(jumpBuffer, 1); // Jump to the point saved by setjmp
+}
+
+void process2 () 
+{
+    std::cout << "Process2 started" << std::endl;
+    std::cout << "Process2 jumping back to process1" << std::endl;
+    longjmp(jumpBuffer, 1); // Jump to the point saved by setjmp
+}
+
+
+int main ()
+{
+    std::cout << "Starting main" << std::endl;
+
+    process1();
+    process2();
+
+    return 0;
+}
+
+/*
+Notes: 
+- Output:
+Starting main
+Process1 started
+process1 jumping to process2 (because jumpbuffer is 0)
+process1 after longjmp (because after going to process 2, it's not 0)
+Process2 started
+Process2 jumping back to process1
+process1 after longjmp
+
+- Overall: Very error prone and hard to utilise so not recommended
+- Also would be very hard to debug in large progammes
+- Better to use proper control flow mechanisms
+
+*/
