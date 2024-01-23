756a430ba03e3f1867b149a9132eabb52336de68
SaadA24 <saadahm1997@gmail.com>
2024-01-23 10:34:55 +0000

    learned about stdarg.h

---
+++ C_Library/c_stdarg.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stdarg.h>
+
+int sum(int count, ...)
+{
+    va_list arguments; // Traverse arguments
+    va_start(arguments, count); //Initialize args to point to the first unnamed argument
+
+int total = 0;
+
+for(int i = 0; i < count; i++)
+{
+    int nextArgument = va_arg(arguments, int); //next argument of int type
+    total += nextArgument;
+}
+
+va_end(arguments); // clean up the va list
+
+return total;
+}
+
+int main() {
+    int result1 = sum(3, 1, 2, 3);
+    int result2 = sum(5, 10, 20, 30, 40, 50);
+
+    std::cout << "Result 1: " << result1 << std::endl;
+    std::cout << "Result 2: " << result2 << std::endl;
+
+    return 0;
+}
+
+/*
+Notes:
+- Main use cases are for printf and scanf so they can take various arguments to format the input/output
+- Remember variable functions can be error prone and function doesn't state how many arguments
+*/
